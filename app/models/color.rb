817e154947f70b6539637e5f17cce0b61626d4e4
onemanstartup <onemanstartup@gmail.com>
2013-12-05 12:35:00 +0300

    Update color.rb

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 # Keep colors for search
 class Color < ActiveRecord::Base
-  attr_accessible :search_color_id, :search_factor, :distance, :active
+  # attr_accessible :search_color_id, :search_factor, :distance, :active
 
   belongs_to :colorable, polymorphic: true
   belongs_to :search_color
